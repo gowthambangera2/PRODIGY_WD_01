4019a9d849a4438f633e2ee030212ed12e577fd8
gowthambangera2 <142571273+gowthambangera2@users.noreply.github.com>
2024-04-07 15:06:14 +0530

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-window.addEventListener('scroll', function() {
-    const navbar = document.getElementById('navbar');
-    if (window.scrollY > 50) {
-      navbar.classList.add('scrolled');
+window.onscroll = function() {scrollFunction()};
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("navbar").style.backgroundColor = "#555";
     } else {
-      navbar.classList.remove('scrolled');
+      document.getElementById("navbar").style.backgroundColor = "#333";
     }
-  });
-  
+  }
